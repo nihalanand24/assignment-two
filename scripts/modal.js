338f3c6b7c6ce88964fb6514aa321b27cab9123c
nihalanand <nihalanand@gmail.com>
2021-02-16 02:05:03 -0500

    completed modal popup function

---
+++ scripts/modal.js
@@ -0,0 +1,30 @@
+const showModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelectorAll('.close-modal');
+
+// showModal.addEventListener('click', handleShow);
+
+// closeModal.addEventListener('click', function(){
+//     modal.style.display = 'none';
+// });
+
+showModal.forEach((btn, index) => {
+    const modal = document.querySelector(`#modal-${index + 1}`);
+    btn.addEventListener('click', function() {
+        handleShow(modal);
+    });
+});
+closeModal.forEach((icon, index) => {
+    const modal = document.querySelector(`#modal-${index + 1}`);
+    icon.addEventListener('click', function() {
+        handleClose(modal);
+    });
+});
+
+function handleShow(modal){
+    modal.style.display = 'block'; 
+}
+
+function handleClose(modal){
+    modal.style.display = 'none';
+}
+
